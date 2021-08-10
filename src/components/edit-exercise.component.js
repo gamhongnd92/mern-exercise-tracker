40e529659f236449e64b852f0a345354015359f4
Gam <trant48@student.douglascollege.ca>
2021-08-10 13:57:30 -0700

    push to github

---
+++ src/components/edit-exercise.component.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+export default class EditExercise extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Your are on the Edit Exercise Component</h1>
+            </div>
+        )
+    }
+}
